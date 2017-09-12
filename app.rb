382b03ac184a6a8ad517feff3ed8c2dc0b05ad7a
Daniel Nollette and Kristen Kulha <daniel@>
2017-09-12 10:29:23 -0700

    successfully route responses to failure and success pages

--- app.rb
+++ app.rb
@@ -7,3 +7,15 @@ require('./lib/riddler')
 get('/') do
   erb( :input)
 end
+
+post('/output') do
+  @answer1 = params.fetch("answer1")
+  @answer2 = params.fetch("answer2")
+  @answer3 = params.fetch("answer3")
+  riddler = Riddler.new(@answer1, @answer2, @answer3)
+  if riddler.is_answer?
+    erb(:success_output)
+  else
+    erb(:failure_output)
+  end
+end
